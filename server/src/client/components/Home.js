b2ae9031fc8b82e24571b6c32c41854bee3acf85
MaruniakS <st.maruniak@gmail.com>
2020-03-26 13:39:43 +0200

    first commit

---
+++ server/src/client/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+        <div>It's the best Home</div>
+        <button onClick={() => console.log('Hi, there!')}>Press me</button>
+    </div>
+);
+
+export default Home;
